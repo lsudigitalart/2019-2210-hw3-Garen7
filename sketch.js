cf6fffdf5cab68f28c5b75fe74309c72819de1fb
Garen Crownguard <22408776+Garen7@users.noreply.github.com>
2019-09-12 14:51:10 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,25 @@
+function setup() {
+  createCanvas(400, 400)
+}
+
+var r = 256, g = 256, b = 256, loopNum = 0
+
+function draw(){
+  for(i = 0; i < 100; i += 1){
+    fill(random(r), random(g), random(b))
+    circle(random(width), random(height), random(20))
+  }
+  if(loopNum < 25){
+    r = loopNum%25*10, g = 256-loopNum%25*10, b = 256
+  }
+  else if(loopNum < 50){
+    r = 256, g = loopNum%25*10, b = 256-loopNum%25*10
+  }
+  else if(loopNum < 75){
+    r = 256-loopNum%25*10, g = 256, b = loopNum%25*10
+  }
+  else{
+    loopNum = 0
+  }
+  loopNum += 1
+}
